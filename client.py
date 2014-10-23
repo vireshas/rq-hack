eeafc6e9664ecd7344c66b13c114e5f69f989b33
Viresh <asviresh@gmail.com>
2014-10-23 12:56:12 +0530

    better client

--- client.py
+++ client.py
@@ -1,9 +1,14 @@
 from redis import Redis
 from rq import Queue
 from worker import count_words_at_url
+from w import add
 import time
 
 q = Queue(connection=Redis())
 result = q.enqueue(count_words_at_url, "http://graph.facebook.com/vireshas")
+time.sleep(2)
+print result.result
+
+result = q.enqueue(add, 1, 2)
 time.sleep(2)
 print result.result
